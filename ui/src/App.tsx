4e668caa66ac7cf91160fe08c970f9e7a04f8855
fullDev9233 <jimyy9211@gmail.com>
2023-06-12 04:57:11 -0500

    native update

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,27 +2,45 @@ import { useEffect, useCallback } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Counter from "./components/Counter";
+import useIsFire from "./hooks/useIsFire";
 import useSubstrateContext from "./hooks/useSubstrateContext";
 import type { ConnectRes } from './types/SubstrateContext';
 
 function App() {
+  const { isFire } = useIsFire()
   const { setAddress, setIsConnected } = useSubstrateContext();
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && typeof window.fire !== 'undefined') {
-      window.fire.on('accountsChanged', handleAccountChanged);
+    if (isFire) {
+      window.fire.on('accountChanged', handleAccountChanged);
       return () => {
-        window.fire.removeListener('accountsChanged', handleAccountChanged);
+        window.fire.removeListener('accountChanged', handleAccountChanged);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [isFire]);
+
+  useEffect(() => {
+    if (isFire) {
+      window.fire.on('networkChanged', handleNetworkChanged);
+
+      return () => {
+        window.fire.removeListener('networkChanged', handleNetworkChanged);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isFire]);
 
   const handleAccountChanged = useCallback((res: ConnectRes) => {
-    setAddress(res.evmAddress);
+    setAddress(res.nativeAddress);
     setIsConnected(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+
+  const handleNetworkChanged = () => {
+    console.log('Network changed')
+  }
 
   return (
     <BrowserRouter>
